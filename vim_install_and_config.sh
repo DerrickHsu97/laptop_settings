d956c9ca26d45fd4ae1983d09de8a197d85a7e78
xuxiaorui <xuxiaorui@didiglobal.com>
2025-03-23 18:49:51 +0800

    [feat]提交自动化脚本

--- vim_install_and_config.sh
+++ vim_install_and_config.sh
@@ -64,8 +64,7 @@ log_info "========== Done! =========="
 
 log_info "========== Install&Config Nvim... =========="
 if [ ! -d ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]; then
-    git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-     ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+    cp -r ./packer.nvim ~/.local/share/nvim/site/pack/packer/start
     cp -r $CUR_DIR/nvim $HOME/.config
 fi
 log_info "========== Done! =========="
